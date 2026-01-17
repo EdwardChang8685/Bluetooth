1dcd93fbc19480cda879ad3c35873383e7e9244b
Edward <edward820630@gmail.com>
2026-01-17 14:25:56 +0800

    Initial Commit

---
+++ Bluetooth/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Bluetooth
+//
+//  Created by Edward Chang on 2026/01/17.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ BluetoothTests/BluetoothTests.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothTests.swift
+//  BluetoothTests
+//
+//  Created by Edward Chang on 2026/01/17.
+//
+
+import Testing
+@testable import Bluetooth
+
+struct BluetoothTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
